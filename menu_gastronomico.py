aaa4d295b9ff008cdcccdb0f276da254b8064680
jorgegmch <gomezjorge.ch@gmail.com>
2025-10-17 20:24:41 -0500

    feat(cocina china): agregar bebidas recomendadas

--- menu_gastronomico.py
+++ menu_gastronomico.py
@@ -93,6 +93,7 @@ def cocina_china():
     print("Pizza margarita: Masa fina cubierta con tomate, mozzarella, y albahaca.")
     print("Pasta carbonara: Espaguetis en salsa de huevo, queso pecorino, panceta y pimienta.")
     print("Risotto al Funghi: Arroz cremoso con setas.")
+    print("Bebidas recomendadas: Jugo de corozo, fresa o uva")
     
 isActive = True
 while isActive:
