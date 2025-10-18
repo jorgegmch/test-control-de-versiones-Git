9aef32f2dc96ed86680723810e64c6deea2e5205
jorgegmch <gomezjorge.ch@gmail.com>
2025-10-17 20:19:31 -0500

    feat(cocina india): agregar descripción del tipo de cocina

--- menu_gastronomico.py
+++ menu_gastronomico.py
@@ -72,6 +72,9 @@ def cocina_japonesa():
 def cocina_india():
     print("COCINA INDIA")
     print("-" * 50)
+    print("Descripción:" \
+    "La cocina italiana es famosa por sus sabores frescos y autenticos," \
+    "basados en ingredientes simples como tomate, ajo, aceite de oliva y hierbas.")
 
 isActive = True
 while isActive:
