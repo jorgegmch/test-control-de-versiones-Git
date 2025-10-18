bad1e96b1264dfaabaefbec240b6b5207f4d640d
jorgegmch <gomezjorge.ch@gmail.com>
2025-10-17 20:01:25 -0500

    feat(cocina italiana): agregar descripción del tipo de cocina

--- menu_gastronomico.py
+++ menu_gastronomico.py
@@ -30,7 +30,9 @@ def menu_gastronimo():
 def cocina_italaiana():
     print("COCINA ITALIANA")
     print("-" * 50)
-    
+    print("Descripción:" \
+    "La cocina italiana es famosa por sus sabores frescos y autenticos," \
+    "basados en ingredientes simples como tomate, ajo, aceite de oliva y hierbas.")
 
 isActive = True
 while isActive:
