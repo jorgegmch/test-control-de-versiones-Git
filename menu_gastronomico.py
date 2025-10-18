563316467b67f8f1c5658a14a90d0f724b367c05
jorgegmch <gomezjorge.ch@gmail.com>
2025-10-17 20:28:32 -0500

    feat(cocina francesa): agregar bebdidas recomendadas

--- menu_gastronomico.py
+++ menu_gastronomico.py
@@ -107,8 +107,8 @@ def cocina_francesa():
     print("Pizza margarita: Masa fina cubierta con tomate, mozzarella, y albahaca.")
     print("Pasta carbonara: Espaguetis en salsa de huevo, queso pecorino, panceta y pimienta.")
     print("Risotto al Funghi: Arroz cremoso con setas.")
+    print("Bebidas recomendadas: Jugo de corozo, fresa o uva")
 
-    
 isActive = True
 while isActive:
     choice = menu_gastronimo()
