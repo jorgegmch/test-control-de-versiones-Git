d0f2292ce58ffaa098d850a255b5882112866838
jorgegmch <gomezjorge.ch@gmail.com>
2025-10-17 20:42:47 -0500

    feat(postres): bebida recomendada

--- menu_gastronomico.py
+++ menu_gastronomico.py
@@ -163,6 +163,7 @@ def postres():
     print("Pizza margarita: Masa fina cubierta con tomate, mozzarella, y albahaca.")
     print("Pasta carbonara: Espaguetis en salsa de huevo, queso pecorino, panceta y pimienta.")
     print("Risotto al Funghi: Arroz cremoso con setas.")
+    print("Bebidas recomendadas: Café")
 
 isActive = True
 while isActive:
