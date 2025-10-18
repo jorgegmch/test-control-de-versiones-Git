a7fc3864fa39d99716c9c09bc59f985d9c39143d
jorgegmch <gomezjorge.ch@gmail.com>
2025-10-17 20:32:26 -0500

    feat(cocina mediterranea): input para comentarios

--- menu_gastronomico.py
+++ menu_gastronomico.py
@@ -122,7 +122,9 @@ def cocina_mediterranea():
     print("Pasta carbonara: Espaguetis en salsa de huevo, queso pecorino, panceta y pimienta.")
     print("Risotto al Funghi: Arroz cremoso con setas.")
     print("Bebidas recomendadas: Jugo de corozo, fresa o uva")
-    
+    comentario_mdt = input("Cuentanos que plato te ha gustado más: ")
+    print(f"{comentario_mdt} ¡Oh ese plato también nos encanta!")
+
 isActive = True
 while isActive:
     choice = menu_gastronimo()
